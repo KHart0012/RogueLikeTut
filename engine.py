68cf9cedbd45bd1d19d92fafa788e07bf3b3fe4a
Kevin Hart <kevin.hart0012@gmail.com>
2019-04-25 23:31:38 -0500

    Completed part 4 & started part 5

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 import tcod
 from entity import Entity
+from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
@@ -9,10 +10,18 @@ def main():
     screen_height = 50
     map_width = 80
     map_height = 45
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
 
     colors = {
-        'dark_wall' : tcod.Color(0, 0, 100),
-        'dark_ground' : tcod.Color(50, 50, 150)
+        'dark_wall' : tcod.Color(0, 100, 0),
+        'dark_ground' : tcod.Color(50, 150, 50),
+        'light_wall' : tcod.Color(130, 110, 50),
+        'light_ground' : tcod.Color(200, 180, 50)
     }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', tcod.white)
@@ -25,13 +34,20 @@ def main():
     con = tcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+
+    fov_recompute = True
+    fov_map = initialize_fov(game_map)
 
     key = tcod.Key()
     mouse = tcod.Mouse()
 
     while not tcod.console_is_window_closed():
         tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        fov_recompute = False
         tcod.console_flush()
 
         clear_all(con, entities)
@@ -46,6 +62,8 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+
+                fov_recompute = True
         
         if ext:
             return True
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,4 +1,8 @@
+import tcod
+from random import randint
+from entity import Entity
 from map_objects.tile import Tile
+from map_objects.rectangle import Rect
 
 class GameMap:
     def __init__(self, width, height):
@@ -7,17 +11,76 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
     
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room):
+        rooms = []
+        num_rooms = 0
+
+        for r in range(max_rooms):
+            w = randint(room_min_size, room_max_size)
+            h = randint(room_min_size, room_max_size)
+            x = randint(0, map_width - w - 1)
+            y = randint(0, map_height - h - 1)
+
+            new_room = Rect(x, y, w, h)
+
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+            else:
+                # No intersections
+                self.create_room(new_room)
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    player.x = new_x
+                    player.y = new_y
+                else:
+                    (prev_x, prev_y) = rooms[num_rooms - 1].center()
+                    if randint(0, 1) == 1:
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+                self.place_entities(new_room, entities, max_monsters_per_room)
+                rooms.append(new_room)
+                num_rooms += 1
+        
+
+    def create_room(self, room):
+        # Go through tiles in the rectangle and make them passable
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        num_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(num_monsters):
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', tcod.desaturated_green)
+                else:
+                    monster = Entity(x, y, 'T', tcod.darker_green)
+                entities.append(monster)
+
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
             return True
